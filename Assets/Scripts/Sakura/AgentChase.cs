0d00105f263dacf4acbcf6ac3dde7686afa84f8a
MylesSteel <90920295+MylesSteel@users.noreply.github.com>
2022-03-20 00:41:48 -0600

    Nav agent
    Nav agent chasing with script
    Nav mesh set
    trigger with animation still an issue. Not the trigger but the animation controls.

---
+++ Assets/Scripts/Sakura/AgentChase.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Normal.Realtime;
+using System;
+public class AgentChase : MonoBehaviour
+{
+    [SerializeField] GameObject _player;      //do it with a tag later
+    [SerializeField] NavMeshAgent _agent;
+    [SerializeField] Realtime _rt;
+    [SerializeField] RealtimeTransform _rtTransform;
+    [SerializeField] GameObject _enemy;
+    [SerializeField] Animator _attack;
+    Vector3 _target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(_enemy.transform.position, _player.transform.position) < 100) //add tag for war on empty
+        {
+            _agent.SetDestination(_player.transform.position);
+            
+           
+        }
+        else
+        {
+            if (Vector3.Distance(_enemy.transform.position, _player.transform.position) < 1)
+            {
+                _attack.Play("fall", 1);
+            }
+        }
+
+    }
+}
